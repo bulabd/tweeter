70aece02c63c01a8ee38acc2d367bbfab8ebb126
bulabd <bulabd@outlook.com>
2022-04-26 19:37:36 +0000

    Use jQuery to respond to the character count change

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  $('#tweet-text').on('input', function() {
+    let count = $("#tweet-text").val().length;
+    let counter = 140 - count;
+    $('output').text(counter);
+    if (counter < 0) {
+      $('output').css('color', 'red');
+    } else {
+      $('output').css('color', '#545149');
+    }
+  })
 });
