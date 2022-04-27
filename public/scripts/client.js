f6d514176d85f1f1040a796be7e1682d6503c21f
bulabd <bulabd@outlook.com>
2022-04-27 14:16:55 +0000

    Implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,18 +4,49 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  // const tweetData = {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //   "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //   "created_at": 1461116232227
+  // };
+  
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
   };
-  
+
   const createTweetElement = function(object) {
     return (
     `<article class="tweet">
@@ -41,12 +72,10 @@ $(document).ready(function() {
     );
   };
   
-  const $tweet = createTweetElement(tweetData);
-  
-  console.log($tweet);
-  
-  $('#tweets-container').append($tweet);
-
+  // const $tweet = createTweetElement(tweetData);
+  // console.log($tweet);
+  // $('#tweets-container').append($tweet);
+  renderTweets(data);
 });
 
 
